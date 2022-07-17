20a7c22f6a3eacd2bb1881dec62ed6331f30ea62
Ардуанов Данил <danil.a@digital-spectr.ru>
2022-07-17 21:45:39 +0500

    task_1.6 | Delete ticket

--- app/Http/Controllers/Lk/TicketsController.php
+++ app/Http/Controllers/Lk/TicketsController.php
@@ -81,8 +81,11 @@ class TicketsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Ticket $ticket)
     {
+
+        $ticket->delete();
+        return response()->noContent();
 
     }
 }
